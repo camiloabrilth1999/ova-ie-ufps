ff1f82fa94b8812be00a750e98c0fde3f585836c
CAMILO <camiloabrilth1999@gmail.com>
2021-06-05 14:02:03 -0500

    Implementing: Custom devise views and controllers were created.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+  devise_for :users, 
+  path: "", 
+  controllers: { 
+    sessions: "users/sessions", 
+    registrations: "users/registrations", 
+    passwords: "users/passwords",
+    confirmations: "users/confirmations" 
+  }, 
+  path_names: { 
+    sign_in: 'login', 
+    password: 'forgot', 
+    confirmation: 'confirm', 
+    unlock: 'unblock', 
+    sign_up: 'register',
+    sign_out: 'signout'
+  }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 end
---
+++ db/migrate/20210605182819_add_attributes_to_users.rb
@@ -0,0 +1,8 @@
+class AddAttributesToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :lastname, :string
+    add_column :users, :code, :string
+    add_column :users, :birth_date, :date
+  end
+end
